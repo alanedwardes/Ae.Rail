5c1280fd73b482b6a2dea827b50bfc8986e9f7b1
Alan Edwardes <alan@alanedwardes.com>
2025-11-17 01:04:41 +0000

    Make other fields nullable.

--- Models/TrainServiceView.cs
+++ Models/TrainServiceView.cs
@@ -4,22 +4,22 @@ namespace Ae.Rail.Models
 {
 	public sealed class TrainServiceView
 	{
-		public string OperationalTrainNumber { get; set; }
-		public string ServiceDate { get; set; } // yyyy-MM-dd
-		public string OriginStd { get; set; } // HH:mm
-		public string Sta { get; set; } // HH:mm
-		public string OriginLocationPrimaryCode { get; set; }
-		public string DestLocationPrimaryCode { get; set; }
-		public string OriginLocationName { get; set; }
-		public string DestLocationName { get; set; }
+		public string? OperationalTrainNumber { get; set; }
+		public string? ServiceDate { get; set; } // yyyy-MM-dd
+		public string? OriginStd { get; set; } // HH:mm
+		public string? Sta { get; set; } // HH:mm
+		public string? OriginLocationPrimaryCode { get; set; }
+		public string? DestLocationPrimaryCode { get; set; }
+		public string? OriginLocationName { get; set; }
+		public string? DestLocationName { get; set; }
 		public DateTime? TrainOriginDateTime { get; set; }
 		public DateTime? TrainDestDateTime { get; set; }
-		public string ToiCore { get; set; }
-		public string ToiVariant { get; set; }
+		public string? ToiCore { get; set; }
+		public string? ToiVariant { get; set; }
 		public int? ToiTimetableYear { get; set; }
 		public DateTime? ToiStartDate { get; set; }
-		public string RailClasses { get; set; }
-		public string PowerType { get; set; }
+		public string? RailClasses { get; set; }
+		public string? PowerType { get; set; }
 		public DateTime LastUpdatedAt { get; set; }
 	}
 }
--- Models/VehicleView.cs
+++ Models/VehicleView.cs
@@ -4,31 +4,31 @@ namespace Ae.Rail.Models
 {
 	public sealed class VehicleView
 	{
-		public string VehicleId { get; set; }
-		public string SpecificType { get; set; }
-		public string TypeOfVehicle { get; set; }
+		public string? VehicleId { get; set; }
+		public string? SpecificType { get; set; }
+		public string? TypeOfVehicle { get; set; }
 		public int? NumberOfCabs { get; set; }
 		public int? NumberOfSeats { get; set; }
-		public string LengthUnit { get; set; }
+		public string? LengthUnit { get; set; }
 		public int? LengthMm { get; set; }
 		public int? Weight { get; set; }
 		public int? MaximumSpeed { get; set; }
-		public string TrainBrakeType { get; set; }
-		public string Livery { get; set; }
+		public string? TrainBrakeType { get; set; }
+		public string? Livery { get; set; }
 		public string? Decor { get; set; }
-		public string VehicleStatus { get; set; }
-		public string RegisteredStatus { get; set; }
-		public string RegisteredCategory { get; set; }
+		public string? VehicleStatus { get; set; }
+		public string? RegisteredStatus { get; set; }
+		public string? RegisteredCategory { get; set; }
 		public DateTime? DateRegistered { get; set; }
 		public DateTime? DateEnteredService { get; set; }
 		public int? ResourcePosition { get; set; }
-		public string PlannedResourceGroup { get; set; }
-		public string ResourceGroupId { get; set; }
-		public string FleetId { get; set; }
-		public string TypeOfResource { get; set; }
+		public string? PlannedResourceGroup { get; set; }
+		public string? ResourceGroupId { get; set; }
+		public string? FleetId { get; set; }
+		public string? TypeOfResource { get; set; }
 		public bool IsLocomotive { get; set; }
-		public string ClassCode { get; set; }
-		public string PowerType { get; set; }
+		public string? ClassCode { get; set; }
+		public string? PowerType { get; set; }
 		public bool IsDrivingVehicle { get; set; }
 		public DateTime LastUpdatedAt { get; set; }
 	}
